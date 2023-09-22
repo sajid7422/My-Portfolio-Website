729447c75686aa2450643e6489e54c2172730b1b
SAJID ANSARI <sajid243502@gmail.com>
2023-09-22 17:42:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let email = document.getElementById("inputEmail4");
+let password = document.getElementById("inputPassword4");
+let address = document.getElementById("inputAddress");
+let city = document.getElementById("inputCity");
+let state = document.getElementById("inputState");
